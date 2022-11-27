6e56a9aada3a7518c1513706355d5a92d8315b55
CrunchXnoy <baby123kucing@gmail.com>
2022-11-27 11:13:32 +0700

    session

--- pages-blank.php
+++ pages-blank.php
@@ -1,3 +1,6 @@
+<?php
+require "proses/session.php";
+?>
 <!DOCTYPE html>
 <html lang="en">
 
---
+++ proses/session.php
@@ -0,0 +1,12 @@
+<?php
+    session_start();
+    require "koneksi.php";
+    if(empty($_SESSION['email'])){
+      echo "<script>window.location='pages-login.php';</script>";
+    }else{
+      $hasil = mysqli_query($conn,"select * from umkm WHERE email='$_SESSION[email]'");
+      $result = mysqli_query($conn, "SELECT * FROM umkm");
+      $row = mysqli_fetch_array($hasil);
+      }
+      
+?>
